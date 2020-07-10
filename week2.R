11bd19c041bc76a5d824da36e0ad37d8ac8a1106
iulisesq <iulisesq@hotmail.com>
2020-07-09 18:28:47 -0600

    Week 2 - Complete

--- week2.R
+++ week2.R
@@ -1,3 +1,5 @@
+############  This is the first:
+
 ##Create a new oauth:
 ##1. in Github
 ##          Public profile
@@ -8,8 +10,6 @@
 ##    Authorization callback URL: http://localhost:1410
 ##
 ##3.  Save and get the Client ID and the Client Secret to use in the first step.
-
-
 
 myapp <- oauth_app("github",
       key = "xxxxxxxxxxxxxxxxx",
@@ -24,3 +24,25 @@ stop_for_status(req)
 output <- content(req)
 datashare <- which(sapply(output, FUN=function(X) "datasharing" %in% X))
 list(output[[datashare]]$name, output[[datashare]]$created_at)
+
+############  This is the second:
+fileUrl <-"https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06pid.csv"
+download.file(fileUrl,destfile="./survey.csv")
+d <- read.csv("./survey.csv")
+sqldf("select pwgtp1 from acs where AGEP<50")
+
+############  This is the third:
+sqldf("select distinct AGEP from acs")
+
+############  This is the fourth:
+library(httr)
+HTML = url("http://biostat.jhsph.edu/~jleek/contact.html")
+htmlCode = readLines(HTML)
+c(nchar(htmlCode[10]),nchar(htmlCode[20]),nchar(htmlCode[30]),nchar(htmlCode[100]))
+
+############  This is the fourth:
+library(dplyr)
+uRl_5 = "https://d396qusza40orc.cloudfront.net/getdata%2Fwksst8110.for"
+data <- read.fwf(file = uRl_5,skip=4, widths=c(12,7,4,9,4,9,4,9,4))
+c4 <- as.numeric(data[,4])
+sum(c4, na.rm=T)
